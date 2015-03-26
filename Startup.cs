440b435468f9133f24e3354dd2303a52f658d9c2
Edmond Leylo <edmondlaylo@gmail.com>
2015-03-26 10:25:20 +0200

    First commit

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dbtest.Startup))]
+namespace dbtest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
